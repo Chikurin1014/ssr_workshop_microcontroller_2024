c870d0fcc184594dcf58ffd03f6dc89469f64de9
Chikurin1014 <chikurin1014@gmail.com>
2024-05-02 03:00:50 +0900

    add a file to test the github actions

---
+++ example/test.hpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+#include "DigitalOut.hpp"
+#include "PwmOut.hpp"
+
+DigitalOut led1 = {2, 0};
+PwmOut                               led2 = {2, 0, 
+20000, 0};
+
+void setup() {
+                                    led1.setup();
+    led2.setup();
+}
+
+void loop() {
+    float duty = led2.getDuty();
+                                                   led1.toggle();
+                   led2.setDuty(duty + 0.2);
+    delay(1000);
+}
