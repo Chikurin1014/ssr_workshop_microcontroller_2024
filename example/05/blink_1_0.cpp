b15a9054a9f88d6245406c21410dff81b71d08d3
DESKTOP-J7UEU6D\daichi <tokyotechdai20@gmail.com>
2024-05-07 13:48:42 +0900

    01でえるちか

---
+++ example/05/blink_1_0.cpp
@@ -0,0 +1,14 @@
+#include <arduino.h>
+
+const int ledPin = 2;
+
+void setup() {
+    pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(ledPin,1);                                                                                    //          HIGH →　1
+    delay(1000);
+    digitalWrite(ledPin, 0);                                             //          LOW →　0
+    delay(1000);
+}
