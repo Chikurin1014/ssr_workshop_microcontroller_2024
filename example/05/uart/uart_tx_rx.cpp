f2ae7cc0c735dcf0ef061e2fb92dc54709604055
DESKTOP-J7UEU6D\daichi <tokyotechdai20@gmail.com>
2024-05-07 13:59:25 +0900

    uartのぷろぐらむ

---
+++ example/05/uart/uart_tx_rx.cpp
@@ -0,0 +1,12 @@
+# include <Arduino.h>
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(Serial.available() > 0) {
+    char data = Serial.read();
+    Serial.println(data);
+  }
+}
