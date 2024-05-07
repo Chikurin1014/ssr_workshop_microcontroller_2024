66fcb893c52f0c7935218c016b297a8ce593cc40
DESKTOP-J7UEU6D\daichi <tokyotechdai20@gmail.com>
2024-05-07 17:20:16 +0900

    わすれた

--- example/05/uart/uart_tx_rx.cpp
+++ example/05/uart/uart_tx_rx.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(9600); // シリアル通信を開始
 }
 
 void loop() {
-    if (Serial.available() > 0) {
-        char data = Serial.read();
-        Serial.println(data);
+    if (Serial.available() > 0) {  // 受け取ったデータがあれば
+        char data = Serial.read(); // データを読み込んで、dataに代入。
+        Serial.write(&data, 1);    // dataをシリアルモニタに表示
     }
 }
