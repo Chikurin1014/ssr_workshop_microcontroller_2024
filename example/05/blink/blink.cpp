1c1258c5a7b9e918efb64230d2516f8518e15f91
DESKTOP-J7UEU6D\daichi <tokyotechdai20@gmail.com>
2024-05-07 14:10:08 +0900

    せつめい

--- example/05/blink/blink.cpp
+++ example/05/blink/blink.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 
-const int ledPin = 2;// const をつけると、値を変更できない変数になります。
+const int ledPin = 2; // const をつけると、値を変更できない変数になります。
 
 void setup() {
-    pinMode(ledPin, OUTPUT);
+    pinMode(ledPin, OUTPUT); // ledPinを出力ピンに設定
 }
 
 void loop() {
-    digitalWrite(ledPin, HIGH);  // ledPinの電圧をHIGH(3.3V)にする
+    digitalWrite(ledPin, HIGH); // ledPinの電圧をHIGH(3.3V)にする
     delay(1000);
-    digitalWrite(ledPin, LOW);   // ledPinの電圧をLOW(0.0V)にする
+    digitalWrite(ledPin, LOW); // ledPinの電圧をLOW(0.0V)にする
     delay(1000);
 }
